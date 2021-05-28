701facc02dbe0e820f67a58b62ecadcb76f07642
Platinumlist <74309052+Platinumlist@users.noreply.github.com>
2021-05-28 11:58:51 +0300

    [Feature][B2CANDROID-727] added user phone number and changed domen .net to .lc

--- app/src/main/java/com/platinumlist/sample/remote/data/common/Storage.kt
+++ app/src/main/java/com/platinumlist/sample/remote/data/common/Storage.kt
@@ -1,6 +1,16 @@
 package com.platinumlist.sample.remote.data.common
 
+import com.platinumlist.sdk.common.Language
+import com.platinumlist.sdk.common.SdkType
+
 object Storage {
     var token: String = ""
     var showId: Long = 0
+    var sdkType: SdkType = SdkType.PURCHASE
+    var lang: String = Language.ENGLISH.fullName
+    var eventShowId: Long = -1L
+    var isEventShowSetting: Int = 1
+    var orderId: Long = -1L
+    var amount: Float = 0f
+    var userPhoneNumber: String = ""
 }
