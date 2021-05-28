74e6f9a2f2f1ecfe0213bf6fdebce10923240543
Platinumlist <74309052+Platinumlist@users.noreply.github.com>
2021-05-28 12:00:12 +0300

    possibility to set user not at the beginning

--- app/src/main/java/com/platinumlist/sample/common/Adapter.kt
+++ app/src/main/java/com/platinumlist/sample/common/Adapter.kt
@@ -3,6 +3,7 @@ package com.platinumlist.sample.common
 import android.content.Intent
 import com.platinumlist.sample.remote.data.common.Storage
 import com.platinumlist.sdk.PlatinumView
+import com.platinumlist.sdk.common.SdkType
 import com.platinumlist.sdk.models.UserRequest
 
 class Adapter(
@@ -27,21 +28,57 @@ class Adapter(
         return Storage.token
     }
 
-    override suspend fun provideIdEventShow(): Long {
+    override suspend fun provideIdEvent(): Long {
         return Storage.showId
     }
 
-    override suspend fun provideUser(): UserRequest {
+    override suspend fun provideEventShowSetting(): Int {
+        return Storage.isEventShowSetting
+    }
+
+    override suspend fun provideIdEventShow(): Long {
+        return Storage.eventShowId
+    }
+
+    override suspend fun provideUser(): UserRequest? {
         return UserRequest(
             name = "User Userovich",
             cityId = 1,
             email = "email@gmail.com",
             nationalityId = 254,
-            phone = "+971501234567"
+            phone = if (Storage.userPhoneNumber.isEmpty()) "+971501234567" else Storage.userPhoneNumber
         )
     }
 
-    override fun orderStatusResolver(orderId: Int, status: String) {
+    override suspend fun provideLanguage(): String {
+        return Storage.lang
+    }
+
+    override fun orderStatusResolver(orderId: Long, status: String) {
         show("orderStatusResolver(orderId: $orderId, status: $status)")
+    }
+
+    override fun closeWebview() {
+        show("closeWebview()")
+    }
+
+    override fun getSdkType(): SdkType {
+        return Storage.sdkType
+    }
+
+    override fun triggerError(message: String, code: Int) {
+        show("triggerError(message: $message, code: $code)")
+    }
+
+    override fun setOrderId(orderId: Long) {
+        Storage.orderId = orderId
+    }
+
+    override fun setOrderAmount(amount: Float) {
+        Storage.amount = amount
+    }
+
+    override fun registerUser() {
+        show("registerUser()")
     }
 }
--- sdk/src/main/java/com/platinumlist/sdk/PlatinumView.kt
+++ sdk/src/main/java/com/platinumlist/sdk/PlatinumView.kt
@@ -3,16 +3,17 @@ package com.platinumlist.sdk
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Environment
 import android.util.AttributeSet
+import android.view.View
 import android.webkit.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.platinumlist.sdk.common.InvalidDataException
-import com.platinumlist.sdk.common.Storage
-import com.platinumlist.sdk.common.WebInterface
 import com.platinumlist.sdk.di.ViewModule
 import com.platinumlist.sdk.models.BasketRequest
 import com.platinumlist.sdk.models.UserRequest
@@ -23,6 +24,15 @@ import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.KodeinTrigger
 import org.kodein.di.generic.instance
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import com.platinumlist.sdk.common.*
+import com.platinumlist.sdk.common.Storage
+import com.platinumlist.sdk.common.WebInterface
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PlatinumView @JvmOverloads constructor(
@@ -53,7 +63,10 @@ class PlatinumView @JvmOverloads constructor(
         WebInterface(
             mCheckOut = this::checkOut,
             mGoBack = this::goBack,
-            mOrderStatusResolver = this::orderStatusResolver
+            mOrderStatusResolver = this::orderStatusResolver,
+            mCloseWebview = this::closeWebview,
+            mTriggerError = this::triggerError,
+            mProcessTicketDownload = this::processTicketDownload
         )
     }
 
@@ -65,6 +78,11 @@ class PlatinumView @JvmOverloads constructor(
 
     init {
         inflate(context, R.layout.view_platinum, this)
+
+        btnBack.setOnClickListener{
+            adapter?.progressSrc?.postValue(true)
+            webView.goBack()
+        }
     }
 
     private inner class MyChromeClient : WebChromeClient() {
@@ -99,10 +117,20 @@ class PlatinumView @JvmOverloads constructor(
         }
     }
 
-    private inner class MyClient: WebViewClient(){
+    private inner class MyClient : WebViewClient() {
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
             adapter?.progressSrc?.postValue(false)
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            if (url?.contains("platinumlist") == true) {
+                toolbar.visibility = View.GONE
+            } else {
+                toolbar.visibility = View.VISIBLE
+            }
         }
     }
 
@@ -142,48 +170,188 @@ class PlatinumView @JvmOverloads constructor(
 
     fun updateUrl(id: Long) {
         CookieManager.getInstance()
-            .setCookie("http://api.platinumlist.net/", "Api-Authorization=Bearer ${Storage.token}")
+            .setCookie("https://api.platinumlist.net/", "Api-Authorization=Bearer ${Storage.token}")
+        CookieManager.getInstance()
+            .setCookie("https://.platinumlist.net/", "user_lang=${Storage.lang}")
 
-        "https://api.platinumlist.net/v/6/event-shows/$id/ticket-office".let {
+        when {
+            Storage.showId != -1L -> {
+                startEvent(Storage.showId)
+            }
+            Storage.eventShowId != -1L -> {
+                startEventShow(Storage.eventShowId, Storage.eventShowSetting)
+            }
+            else -> {
+                adapter?.triggerError("Incorrect event id", 1)
+            }
+        }
+    }
+
+    private fun startEvent(idEvent: Long) {
+        "https://api.platinumlist.net/v/7/events/$idEvent/ticket-office?sdk_type=${adapter?.getSdkType()?.ordinal}".let {
             webView.loadUrl(
                 it,
                 mapOf(
-                    "Api-Authorization" to "Bearer ${Storage.token}"
+                    "Api-Authorization" to "Bearer ${Storage.token}",
+                    "user_lang" to "${Storage.lang}"
                 )
             )
         }
     }
 
-    private fun checkOut(link: String) {
+    private fun startEventShow(idEventShow: Long, eventShowSetting: Int) {
+        "https://api.platinumlist.net/v/7/event-shows/$idEventShow/ticket-office?sdk_type=${adapter?.getSdkType()?.ordinal}&event_show_setting=$eventShowSetting".let {
+            webView.loadUrl(
+                it,
+                mapOf(
+                    "Api-Authorization" to "Bearer ${Storage.token}",
+                    "user_lang" to "${Storage.lang}"
+                )
+            )
+        }
+    }
+
+    private fun checkOut(link: String, showId: Long) {
         CoroutineScope(Dispatchers.IO).launch {
+            val user = adapter?.provideUser() ?: Storage.user
+
+            if (user == null) {
+                adapter?.registerUser()
+                return@launch
+            }
+
             api.changeBasketAsync(
-                id = Storage.showId,
+                id = showId,
                 request = BasketRequest(
                     scope = "partner-sale.event-tickets",
                     include = "user",
                     operation = "set.user",
-                    user = Storage.user!!
+                    user = user
                 )
             ).await().let {
-                when (it.code()) {
-                    401 -> InvalidDataException(InvalidDataException.Data.TOKEN).let {
-                        adapter?.checkOutError(it)
-                    }
-                    400 -> InvalidDataException(InvalidDataException.Data.ID_EVENT_SHOW).let {
-                        adapter?.checkOutError(it)
-                    }
-                    else -> {
-                        withContext(Dispatchers.Main) {
-                            webView.loadUrl(link)
-                        }
-                    }
+                withContext(Dispatchers.Main) {
+                    webView.loadUrl(link)
                 }
             }
         }
     }
 
-    private fun orderStatusResolver(orderId: Int, status: String) {
+    private fun orderStatusResolver(orderId: Long, status: String) {
+        if (adapter?.getSdkType() == SdkType.TICKET_OFFICE) {
+            CoroutineScope(Dispatchers.IO).launch {
+                withContext(Dispatchers.Main) {
+                    webView.loadUrl("javascript:triggerApiEvent('checkOrderStatus')")
+                }
+            }
+        }
+
         adapter?.orderStatusResolver(orderId, status)
+
+        if (adapter?.getSdkType() == SdkType.PURCHASE || adapter?.getSdkType() == SdkType.SELECT_TICKETS) {
+            if (adapter?.getSdkType() == SdkType.SELECT_TICKETS) {
+                adapter?.setOrderId(orderId)
+
+                getOrderAmount(orderId)
+            }
+            clear()
+            goBack()
+        }
+    }
+
+    private fun getOrderAmount(orderId: Long) {
+        GlobalScope.launch {
+            try {
+                api.getOrderAsync(
+                    id = orderId,
+                    scope = "partner-sale.receive-payments"
+                ).await().let {
+                    Storage.amount = it.body()?.data?.amount ?: 0f
+                    adapter?.setOrderAmount(Storage.amount)
+                }
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun closeWebview() {
+        clear()
+        goBack()
+
+        adapter?.closeWebview()
+    }
+
+    private fun triggerError(message: String, code: Int) {
+        adapter?.triggerError(message, code)
+    }
+
+    private fun processTicketDownload(link: String, type: String) {
+
+        when (type) {
+            LinkType.PDF.typeName -> {
+                downloadPDFTicket(link)
+            }
+            LinkType.GOOGLE_WALLET.typeName -> {
+                addToGoogleWallet(link)
+            }
+            LinkType.APPLE_WALLET.typeName -> {
+
+            }
+        }
+    }
+
+    private fun downloadPDFTicket(link: String) {
+        adapter?.progressSrc?.postValue(true)
+
+        val sdf = SimpleDateFormat("dd-MM-yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        val fileName = "ticket $currentDate.pdf"
+
+        val extStorageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+        val file = File(extStorageDirectory)
+        file.mkdir()
+
+        val pdfFile = File(file, fileName)
+
+        try {
+            pdfFile.createNewFile()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+        downloadTicket(link, pdfFile)
+
+        adapter?.progressSrc?.postValue(false)
+
+        Toast.makeText(context,"File downloaded to $pdfFile",Toast.LENGTH_LONG).show()
+    }
+
+    private fun addToGoogleWallet(link: String) {
+        val browserIntent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(context, browserIntent, null)
+    }
+
+    private fun downloadTicket(link: String, directory: File) {
+        try {
+            val url = URL(link)
+            val urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.connect()
+
+            val inputStream = urlConnection.inputStream
+            val fileOutputStream = FileOutputStream(directory)
+
+            val buffer = ByteArray(1024 * 1024)
+            var bufferLength: Int
+            while (inputStream.read(buffer).also { bufferLength = it } > 0) {
+                fileOutputStream.write(buffer, 0, bufferLength)
+            }
+
+            fileOutputStream.close()
+            urlConnection.disconnect()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
     }
 
     private fun goBack() {
@@ -200,7 +368,7 @@ class PlatinumView @JvmOverloads constructor(
         }
     }
 
-    internal fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+    internal fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_SELECT_FILE) {
             if (uploadMessage == null) {
                 return
@@ -227,10 +395,13 @@ class PlatinumView @JvmOverloads constructor(
          */
         suspend fun invalidate() {
             Storage.token = provideToken()
-            Storage.showId = provideIdEventShow()
+            Storage.showId = provideIdEvent()
             Storage.user = provideUser()
+            Storage.eventShowId = provideIdEventShow()
+            Storage.eventShowSetting = provideEventShowSetting()
+            Storage.lang = provideLanguage()
 
-            view?.check(Storage.showId)
+            progressSrc.postValue(true)
 
             withContext(Dispatchers.Main) {
                 view?.updateUrl(Storage.showId)
@@ -242,12 +413,15 @@ class PlatinumView @JvmOverloads constructor(
         abstract fun goBack()
 
         abstract suspend fun provideToken(): String
+        abstract suspend fun provideIdEvent(): Long
         abstract suspend fun provideIdEventShow(): Long
-        abstract suspend fun provideUser(): UserRequest
+        abstract suspend fun provideEventShowSetting(): Int
+        abstract suspend fun provideUser(): UserRequest?
+        abstract suspend fun provideLanguage(): String
 
-        abstract fun orderStatusResolver(orderId: Int, status: String)
+        abstract fun orderStatusResolver(orderId: Long, status: String)
 
-        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
             view?.onActivityResult(requestCode, resultCode, data)
         }
 
@@ -256,9 +430,16 @@ class PlatinumView @JvmOverloads constructor(
         internal fun clear() {
             view = null
         }
+
+        abstract fun closeWebview()
+        abstract fun getSdkType(): SdkType
+        abstract fun triggerError(message: String, code: Int)
+        abstract fun setOrderId(orderId: Long)
+        abstract fun setOrderAmount(amount: Float)
+        abstract fun registerUser()
     }
 
-    companion object{
+    companion object {
         private const val REQUEST_SELECT_FILE = 189
     }
 }
