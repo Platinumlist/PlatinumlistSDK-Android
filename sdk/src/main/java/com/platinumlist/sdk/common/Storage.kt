91a505f3c4c4883843b9280a68e527c674815bc6
Platinumlist <74309052+Platinumlist@users.noreply.github.com>
2021-05-28 11:59:06 +0300

    [Feature][B2CANDROID727] added update order status to sdk side

--- sdk/src/main/java/com/platinumlist/sdk/common/Storage.kt
+++ sdk/src/main/java/com/platinumlist/sdk/common/Storage.kt
@@ -6,4 +6,9 @@ internal object Storage {
     var token: String = ""
     var showId: Long = -1L
     var user: UserRequest? = null
+    var lang: String = Language.ENGLISH.fullName
+    var eventShowId: Long = -1L
+    var eventShowSetting: Int = 1
+    var orderId: Long = -1L
+    var amount: Float = 0f
 }
--- sdk/src/main/java/com/platinumlist/sdk/remote/Api.kt
+++ sdk/src/main/java/com/platinumlist/sdk/remote/Api.kt
@@ -1,6 +1,10 @@
 package com.platinumlist.sdk.remote
 
+import com.platinumlist.sdk.common.DataWrapper
 import com.platinumlist.sdk.models.BasketRequest
+import com.platinumlist.sdk.request.OrderCompletedRequest
+import com.platinumlist.sdk.request.OrderFailedRequest
+import com.platinumlist.sdk.response.OrderResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.*
@@ -31,8 +35,33 @@ internal interface Api {
         request: BasketRequest
     ): Deferred<Response<Any>>
 
+    @PATCH(ORDER)
+    fun completedOrderAsync(
+        @Path("id")
+        id: Long,
+        @Body
+        request: OrderCompletedRequest
+    ): Deferred<Response<Any>>
+
+    @PATCH(ORDER)
+    fun failedOrderAsync(
+        @Path("id")
+        id: Long,
+        @Body
+        request: OrderFailedRequest
+    ): Deferred<Response<Any>>
+
+    @GET(ORDER)
+    fun getOrderAsync(
+        @Path("id")
+        id: Long,
+        @Query("scope")
+        scope: String
+    ): Deferred<Response<DataWrapper<OrderResponse>>>
+
     companion object {
         private const val CHECK = "event-shows/{id}"
         private const val BASKET = "baskets/{id}"
+        private const val ORDER = "orders/{id}"
     }
 }
